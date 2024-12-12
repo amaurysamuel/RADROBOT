fc37ed4d3028834d974963b7beee9b9bca36bc10
Amaurytus <amaury.samuel@epitech.digital>
2024-12-12 11:23:18 +0100

    script ia

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RADROBOT
+
+c'est un projet qui utilise python=3.11
---
+++ Script.py
@@ -0,0 +1,26 @@
+# Install transformers from source - only needed for versions <= v4.34
+# pip install git+https://github.com/huggingface/transformers.git
+# pip install accelerate
+
+import torch
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
+
+# We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
+messages = [
+    {
+        "role": "system",
+        "content": "You are a friendly and knowledgeable chatbot specializing in radioactivity detection and safety. You provide clear, accurate, and scientifically sound information to users. Your tone is reassuring and professional, ensuring users feel safe and informed when interacting with you.",
+    },
+    {"role": "user", "content": "What is radioactivity?"},
+]
+prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print(outputs[0]["generated_text"])
+# <|system|>
+# You are a friendly chatbot who always responds in the style of a pirate.</s>
+# <|user|>
+# How many helicopters can a human eat in one sitting?</s>
+# <|assistant|>
+# ...
